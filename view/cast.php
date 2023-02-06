f33426f1211b3cb1d705ac23f157d2d94b9de352
Lynn-Mei <hugo.murura@gmail.com>
2023-02-06 14:49:37 +0100

    First version of main page
    The first version of the main page's css, the content still needs to be defined and as such, the layout and css will need to be improved based on the future changes

--- view/cast.php
+++ view/cast.php
@@ -1,71 +1,49 @@
 <html>
 <head>
-	<link rel="stylesheet" href="css/index.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/index.css"/>
 </head>
 <body>
 	<header>
-		<nav>
-			<h1>Rynik</h1>
-			<ul id="nav-pages">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="silverfox.php">Silverfox</a></li>
-				<?php
-					if(isset($_SESSION['rank']))
-					{
-						if($_SESSION['rank']==2)
-						{
-							echo "<li><a href='disconnect.php'>Admin board</a></li>";
-						}
-						echo "<li><a href='disconnect.php'>Log Out</a></li>";
+		<div id="title">
+			<h1>Hakumei-Hime</h1>
+		</div>
 
-					}
-					else
-					{
-						echo "<li><a href='login-page.php'>Login</a></li>";
-						echo "<li><a href='register-page.php'>Sign In</a></li>";
-					}
-				?>
-				
-			</ul>
-
-			<ul id="nav-account-details">
-				<li id="param-box">
-					<img id="cog" src="images/cog.png">
-				</li>
-				<li id="profile-box">
-					<?php 
-						if(isset($_SESSION['pplink']))
-						{
-							if($_SESSION['pplink'] != ''){
-								echo "<img id='profile-img' src='". $_SESSION['pplink'] ."'>";
-							}
-							else{
-								echo "<img id='profile-img' src='images/chicken.png'>";
-							}
-						}else{
-							echo "<img id='profile-img' src='images/chicken.png'>";
-						}
-					
-					?>
-					
-				</li>
-			</ul>
-			<div id="nav-profile-box-div">
-				<?php
-					if(isset($_SESSION['name'])){
-						echo "<p>".$_SESSION['name']."</p>";
-						echo "<a href='account-page.php'> Your Profile </a>";
-
-					}else{
-						echo "<p>You are not connected</p>";
-					}
-				?>
-			</div>
-			<script src="js/profile.js"></script>
-		</nav>
 	</header>
-	<main>
+	</br></br></br></br>
+	<ul id="toolbar">
+		<a href='index.php'><li>Home</li></a>
+		<a href='index.php'><li>Categories</li></a>
+		<a href='index.php'><li>Translator</li></a>
+		<a href='index.php'><li>Language</li></a>
+		<a href='index.php'><li>Cuisine</li></a>
+		<a href='index.php'><li>Sports</li></a>
+		<?php
+			if(isset($_SESSION['rank']))
+			{
+				if($_SESSION['rank']==2)
+				{
+					echo "<a href='index.php'><li>Admin board</li></a>";
+				}
+				echo "<a href='index.php'><li>Log Out</li></a>";
 
+			}
+			else
+			{
+					echo "<a href='index.php'><li>Login</li></a>";
+					echo "<a href='index.php'><li>Sign In</li></a>";
+			}
+		?>
+	</ul>
+	<main>
+		<?= $contenu; ?>
 	</main>
+	<footer>
+		<a>Conditions generales d'utilisation</a>
+		<a>Politique de confidentialite</a>
+		<p>Adress</p>
+		<p>Infos entreprise et blah blah blah</p>
+	</footer>
 </body>
+
 </html>
